40684a2b346277cc182a5eebc572a4af98cd2017
mno90 <131266070+mnder3ii@users.noreply.github.com>
2023-09-11 02:34:26 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
 $("#v-a-wrp").hide();
+$("#c-timel").hide();
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+      // Unobserve the element to prevent further callbacks
+      observer.unobserve(entry.target);
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+}, {
+  // Add the "once" option to the IntersectionObserver configuration
+  once: true
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+
+$(".dropdown-toggle").click(function(){
+  $(".dropdown-menu").toggleClass("show");
+});
 
 $("#close_note").on("click", function () {
   $("#hero1").slideUp(function () {
@@ -30,7 +55,7 @@ function callMe() {
 
 function sendEmail() {
     window.location.href = 'mailto:Contact@mno90.com?subject=Hello&body=I wanted to reach out to you...'; // Replace with your email address, subject, and body
-  }
+}
 
 $("#git_h").click(function() {
     // Open the link in a new tab or window
@@ -71,4 +96,23 @@ $("#ytchnn").click(function() {
 
 
 
+$('#v-cv-2').click(function() {
+  window.location.href = './cv.html';
+});
+
+
+$("#showtimeline").on("click", function () {
+  $("#showtimeline").fadeOut();
+  $("#my-tmtm1").fadeOut(function(){
+    $("#c-timel").fadeIn();
+  });
+});
+
+$('#vv-gain').click(function() {
+  $("#c-timel").fadeOut(function(){
+    $("#my-tmtm1").fadeIn();
+    $("#showtimeline").fadeIn();
+    
+  });
+});
 
